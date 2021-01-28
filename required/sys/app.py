0939e65dcf55c919ea0b92941530a9b2db41225a
Tanaka Mutsatsa <gilbertgrace@icloud.com>
2021-01-28 22:12:29 +0000

    Implemented progress bar for splashscreen

--- required/sys/app.py
+++ required/sys/app.py
@@ -8,7 +8,7 @@ class Exit(Exception): pass
 
 config:dict = {
     'version' : '1.0',
-    'root' : 'E:/Projects/iV Pro',
+    'splash' : 'E:/Projects/iV Pro',
     'etc_path' : 'required/etc',
     'var_path' : 'required/var',
     'connection_string' : 'system.db',
@@ -35,7 +35,7 @@ config:dict = {
 with open('E:/Projects/iV Pro/required/etc/config.json', 'r') as f: config.update(json.loads(f.read()))
 
 for _ in config['required']:
-    if not os.path.join(config['root'], 'required', _) in sys.path: sys.path.append(os.path.join(config['root'], 'required', _))
+    if not os.path.join(config['splash'], 'required', _) in sys.path: sys.path.append(os.path.join(config['splash'], 'required', _))
 
 class Client:
     running:bool
@@ -52,9 +52,10 @@ class Engine:
 
 client:Client = None; engine:Engine = None # processes
 splash:tkinter.Tk = None;  editor:tkinter.Tk = None # windows
+progress:object
 
 def report(message:str, verbose:bool=False): 
-    with open(os.path.join(config['root'], config['etc_path'], config['log_file']), 'a') as log: log.write(f'[{datetime.datetime.now()}]: {message}\n')
+    with open(os.path.join(config['splash'], config['etc_path'], config['log_file']), 'a') as log: log.write(f'[{datetime.datetime.now()}]: {message}\n')
     if verbose: print(message)
 
 def loop() -> None:
@@ -65,40 +66,64 @@ def loop() -> None:
 
 def show():
     ''' Loads the editor '''
-    global splash
+    import ttk
+    global splash, progress
     splash = tkinter.Tk()
     splash.title('Welcome')
     min_width = 600; min_height = 400
     splash.geometry('%sx%s+0+0' % (min_width, min_height))
     splash.resizable(0, 0)
-    image = tkinter.PhotoImage(file = f"{config['root']}/{config['var_path']}/splash.gif") 
+    image = tkinter.PhotoImage(file = f"{config['splash']}/{config['var_path']}/splash.gif") 
     handle = tkinter.Label( splash, image = image) 
     handle.place(x = 0, y = 0)
     button = tkinter.Button(splash, text ='Launch', command = start)
     button.pack()
+    progress = ttk.Progressbar(splash, orient = tkinter.HORIZONTAL, length = 100, mode = 'determinate')
+    progress.pack(pady = 10)
     splash.wm_attributes('-topmost', True)
-    splash.protocol('WM_DELETE_WINDOW', onexit)
+    splash.protocol('WM_DELETE_WINDOW', closing)
     center_tk_window.center(splash, splash)
     #splash.after_idle(start)
     splash.mainloop()
 
 def start(*args, **kwargs):
-    global engine, editor, splash
+    global engine, editor, splash, progress
     #engine = Engine()
+    progress['value'] = 20
+    splash.update_idletasks() 
+    time.sleep(1) 
+  
+    progress['value'] = 40
+    splash.update_idletasks() 
+    time.sleep(1) 
+  
+    progress['value'] = 50
+    splash.update_idletasks() 
+    time.sleep(1) 
+  
+    progress['value'] = 60
+    splash.update_idletasks() 
+    time.sleep(1) 
+  
+    progress['value'] = 80
+    splash.update_idletasks() 
+    time.sleep(1) 
+    progress['value'] = 100
+
     editor = tkinter.Tk()
     editor.title('iV Pro')
     min_width = 1280; min_height = 720
     editor.geometry('%sx%s+0+0' % (min_width, min_height))
     editor.minsize(min_width, min_height)
     editor.maxsize(editor.winfo_screenwidth(), editor.winfo_screenheight())
-    #image = tkinter.PhotoImage(file = f"{config['root']}/{config['var_path']}/iris.png")
+    #image = tkinter.PhotoImage(file = f"{config['splash']}/{config['var_path']}/iris.png")
     #editor.iconphoto(False, image)
     #editor.wm_attributes("-disabled", True)
     editor.state('zoomed')
     center_tk_window.center(editor, editor)
     splash.destroy()
 
-def onexit(): pass
+def closing(): splash.destroy()
 
 # entry point
 if __name__ == '__main__':
